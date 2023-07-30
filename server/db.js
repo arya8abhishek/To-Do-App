bdaaaa705b79c99b05bd7c69ec97e03b00b4d9b5
arya8abhishek <work8arya@gmail.com>
2023-07-30 22:00:44 +0530

    hello

--- server/db.js
+++ server/db.js
@@ -2,11 +2,18 @@ const Pool = require('pg').Pool
 require('dotenv').config()
 
 const pool = new Pool({
-    user: process.env.PGUSERNAME,
-    password: process.env.PGPASSWORD,
-    host: process.env.HOST,
-    database: 'todoapp',
+    user: "default",
+    password: "pnsOV7dD3GgA",
+    host: "ep-sweet-tooth-660152-pooler.us-east-1.postgres.vercel-storage.com",
+    database: 'verceldb',
     ssl: true
 })
+// const pool = new Pool({
+//     user: process.env.PGUSERNAME,
+//     password: process.env.PGPASSWORD,
+//     host: process.env.HOST,
+//     database: 'todoapp',
+//     ssl: true
+// })
 
 module.exports = pool
